d85b7f59728b1fd5408769611f9ccb888bb6c5fb
soskitty <42536247+soskitty@users.noreply.github.com>
2026-06-26 12:53:04 +0800

    add coordinatorlayout dep

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,7 @@ android {
 dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
